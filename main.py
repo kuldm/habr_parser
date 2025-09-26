69b1b5c763b6dd3f39cdf9525827b5a16635018d
Dmitriy <prizrak2165@gmail.com>
2025-09-26 15:33:21 +0300

    Добавлено извлечение ссылки на статью. Добавлено извлечение текста статьи

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pprint import pprint
 
 
 main_page = "https://habr.com/ru/articles/top/daily/"
+habr_page = "https://habr.com"
 
 def get_url_html(url: str) -> str:
     res = requests.get(
@@ -25,6 +26,16 @@ def get_soup(html_text: str) -> BeautifulSoup:
 class ArticleData:
     title: str
     views: str
+    link: str
+    text: str
+
+
+def get_article_text(article_link: str):
+    article_html = get_url_html(article_link)
+    article_soup = get_soup(article_html)
+    article_body = article_soup.find("div", class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
+    return article_body.text
+
 
 
 def get_all_habr_posts(soup: BeautifulSoup) -> list[ArticleData]:
@@ -33,9 +44,13 @@ def get_all_habr_posts(soup: BeautifulSoup) -> list[ArticleData]:
     for article_soup in all_articles_soup:
         article_title: str = article_soup.find("a", class_="tm-title__link").find("span").text
         article_views = article_soup.find("span", class_="tm-icon-counter__value").text
+        article_link = article_soup.find("a", class_="tm-title__link").get("href")
+        article_text = get_article_text(f"{habr_page}{article_link}")
         posts_data.append(ArticleData(
             title=article_title,
-            views=article_views
+            views=article_views,
+            link=f"{habr_page}{article_link}",
+            text=article_text
         ))
     return posts_data
 
